6be07f7b2ed86196c5fa0f59d90f40dadb231bda
ayush <ayushshivoy06@gmail.com>
2025-07-23 11:03:47 +0530

    First Class

---
+++ new-react/src/App.jsx
@@ -0,0 +1,13 @@
+import First from "./First/First";
+
+function App(){
+  return(
+    <>
+    <h1>Hello React</h1>
+    <First/>
+    </>
+    
+  )
+}
+
+export default App;
---
+++ new-react/src/First/First.jsx
@@ -0,0 +1,6 @@
+export default function First(){
+    return(
+        <h1>First Component</h1>
+    )
+}
+
